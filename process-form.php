b7e7ca620dbb9e784b42e9767ea497b2eccab187
Jesse Mazur <mazureth@gmail.com>
2018-05-03 19:46:43 -0700

    getting the booking form to work

--- process-form.php
+++ process-form.php
@@ -1,6 +1,8 @@
 <?php
 
-$to = 'info@mazureth.com';
+$to = 'mazureth@gmail.com';
+
+$subject = 'MAZURETH CONTACT FORM';
 
 $name = $_POST['name'];
 $from = $_POST['email'];
@@ -18,7 +20,11 @@ Message:
 $message
 ";
 
-if (mail($to, $from, $emailMessage)) {
+$headers = "From: $from" . "\r\n" .
+    "Reply-To: $from" . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+if (mail($to, $subject, $emailMessage)) {
   header("HTTP/1.0 200 OK", false, 200);
 } else {
   header("HTTP/1.0 500 Internal Server Error", false, 500);
