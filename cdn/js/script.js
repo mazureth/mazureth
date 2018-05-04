d5829148956e85c818bfe5b7bda20865693bf87e
Jesse Mazur <mazureth@gmail.com>
2018-05-04 14:50:29 -0700

    site launched version 1.0

--- cdn/js/script.js
+++ cdn/js/script.js
@@ -3,30 +3,40 @@ $(function() {
 var $window = $(window),
     $mixer = $('.mixer'),
     $mainSection = $('.main section'),
-    $navbarNav = $('#navbarNav')
-    $popoverLinks = $('a.pop-over'),
-    $popover = $('#pop-over'),
-    $popoverContent = $('#pop-over .container'),
-    $popoverClose = $('#pop-over .close');
+    $navbarNav = $('#navbarNav');
 
 // nav clicks for active 'tab'
 $('.navbar .nav-item').on('click', function(e){
-   var href = $(this).find('a').attr('href'),
-       scrollTo = $(href)[0].offsetTop;
+   var href, scrollTo,
+       subpages = [
+         '/booking.html',
+         '/clients.html',
+         '/gearlist.html'
+       ];
+
+   // bail if we are on a subpage, links should work as expected
+   if (subpages.indexOf(window.location.pathname) >= 0) { return }
+
+   href = $(this).find('a').attr('href');
+   scrollTo = $(href)[0].offsetTop;
+
    e.preventDefault();
 
    $navbarNav.find('.active').removeClass('active');
    $(this).addClass('active');
-
-   if ($popover.is(':visible')) {
-      $popover.hide();
-   }
 
    window.scroll({
      top: scrollTo,
      left: 0,
      behavior: 'smooth'
    });
+
+   if(history.pushState) {
+     history.pushState(null, null, href);
+   } else {
+    location.hash = '#myhash';
+   }
+
 });
 
 // auto close navbar on click
@@ -37,111 +47,106 @@ $('.navbar-collapse a').click(function(){
 // Scroll listener
 $window.on('scroll', function(e){
   var scroll = $window.scrollTop(),
-      mixerPosition = $mixer.css('backgroundPosition').split(' ');
+      mixerPosition;
 
-  // mixer paralax
-  mixerPosition[1] = 0 + (scroll / 5) + 'px';
-  $mixer.css('backgroundPosition', mixerPosition.join(' '));
+  if ($mixer.length) {
+    mixerPosition = $mixer.css('backgroundPosition').split(' ');
 
-  // dumb scroll spy
-  $mainSection.each(function(){
-    var $this = $(this)[0];
-    if (Math.abs($this.offsetTop-scroll) < 20) {
-      $navbarNav.find('.active').removeClass('active');
-      $navbarNav.find('a[href="#' + $this.id + '"]').addClass('active');
-    }
-  });
+    // mixer paralax
+    mixerPosition[1] = 0 + (scroll / 5) + 'px';
+    $mixer.css('backgroundPosition', mixerPosition.join(' '));
 
-});
-
-// popover box
-var alreadyFetched = {};
-function managePopover(fetch) {
-  var content = alreadyFetched[fetch] || '';
-
-  if ($popover.is(':visible') && content) {
-    $popoverContent.html(content);
-  } else if ($popover.not(':visible') && content) {
-    $popoverContent.html(content);
-    $popover.show();
-  } else {
-    $popover.hide();
-  }
-};
-$popoverLinks.on('click', function(e){
-  var fetch = $(this).data('fetch');
-  e.preventDefault();
-  // if we havent already fetched this, do so now
-  if (!alreadyFetched[fetch]) {
-    // fetch the data for the extra content
-    $.ajax({
-      url: fetch,
-      success: function(data) {
-        alreadyFetched[fetch] = data;
-        managePopover(fetch);
-      },
-      dataType: 'html'
+    // dumb scroll spy
+    $mainSection.each(function(){
+      var $this = $(this)[0];
+      if (Math.abs($this.offsetTop-scroll) < 20) {
+        $navbarNav.find('.active').removeClass('active');
+        $navbarNav.find('a[href="#' + $this.id + '"]').addClass('active');
+      }
     });
-  } else {
-    managePopover(fetch);
   }
-});
-$popoverClose.on('click', function(e){
-  e.preventDefault();
-  $popover.hide();
 });
 
 // form handler
-$(document).on('click', '#submit', function(e){
-  e.preventDefault();
+$('form').submit( function(e) {
   var $name = $('#name'),
       $email = $('#email'),
       $startDate = $('#start-date'),
       $endDate = $('#end-date'),
-      $message = $('#message');
+      $message = $('#message'),
+      $captchaBox = $('.g-recaptcha :first-child')[0],
+      isHuman = grecaptcha.getResponse(),
+      defaultPrevented = false;
+
+  // make sure its not a bot
+  if (!isHuman) {
+    $('.g-recaptcha :first-child').first().css('border','1px solid red');
+    defaultPrevented = true;
+    e.preventDefault();
+  } else {
+    $('.g-recaptcha :first-child').first().css('border','none');
+  }
 
   if (!$name.val().trim()) {
     $name.removeClass('is-valid');
     $name.addClass('is-invalid');
+    defaultPrevented = true;
+    e.preventDefault();
   } else {
     $name.removeClass('is-invalid');
     $name.addClass('is-valid');
   }
+
   if (!$email.val().match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)) {
     $email.removeClass('is-valid');
     $email.addClass('is-invalid');
+    defaultPrevented = true;
+    e.preventDefault();
   } else {
     $email.removeClass('is-invalid');
     $email.addClass('is-valid');
   }
+
   if (!$startDate.val().match(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/)) {
     $startDate.removeClass('is-valid');
     $startDate.addClass('is-invalid');
+    defaultPrevented = true;
+    e.preventDefault();
   } else {
     $startDate.removeClass('is-invalid');
     $startDate.addClass('is-valid');
   }
+
   if (!$endDate.val().match(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/)) {
     $endDate.removeClass('is-valid');
     $endDate.addClass('is-invalid');
+    defaultPrevented = true;
+    e.preventDefault();
   } else {
     $endDate.removeClass('is-invalid');
     $endDate.addClass('is-valid');
   }
+
   if (!$message.val().trim()) {
     $message.removeClass('is-valid');
     $message.addClass('is-invalid');
+    defaultPrevented = true;
+    e.preventDefault();
   } else {
     $message.removeClass('is-invalid');
     $message.addClass('is-valid');
   }
-  $.ajax({
-    url: './process-form.php',
-    success: function(data) {
-      var template = `<h1 class="text-center">THANKS</h1><p>Thank you for contacting us ${name}! We will get back to you as soon as we can.`;
-      $popoverContent.html(template);
-    }
-  });
+
+  if (!defaultPrevented) {
+    $(this).unbind('submit').submit();
+  }
 });
+
+var hash = window.location.hash;
+if (hash === "#thanks") {
+  $('#userMessage').html('Thank you for contacting us. We will get back to you as soon as possible');
+} else if (hash === '#error') {
+  $('#userMessage').html('There was an error submitting the form. Please try again.');
+}
 
 });
--- process-form.php
+++ process-form.php
@@ -5,16 +5,16 @@ $to = 'mazureth@gmail.com';
 $subject = 'MAZURETH CONTACT FORM';
 
 $name = $_POST['name'];
-$from = $_POST['email'];
-$start = $_POST['startDate'];
-$end = $_POST['endDate'];
+$email = $_POST['email'];
+$startDate = $_POST['start-date'];
+$endDate = $_POST['end-date'];
 $message = $_POST['message'];
 
 $emailMessage = "
 Name: $name
 Email: $email
-Start Date: $start
-End Date: $end
+Start Date: $startDate
+End Date: $endDate
 
 Message:
 $message
@@ -26,8 +26,11 @@ $headers = "From: $from" . "\r\n" .
 
 if (mail($to, $subject, $emailMessage)) {
   header("HTTP/1.0 200 OK", false, 200);
+  header('Location: ./booking.html#thanks');
 } else {
   header("HTTP/1.0 500 Internal Server Error", false, 500);
+  header('Location: ./booking.html#error');
 }
+
 
 ?>
