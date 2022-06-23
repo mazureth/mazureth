9b8de3976da49e1d02c86dbcc7eddfe28df97c0d
Jesse Mazur <jessemazur@fb.com>
2022-06-23 14:42:49 -0700

    Moving the SendGrid processing to inside the error check so that it requres the key to be present before it attempts to add to the email list. This should stop the spamming in SendGrid and only allow validated customers to post (similar to how only validated cusotmers generate an email to me)

--- process-form.php
+++ process-form.php
@@ -57,38 +57,39 @@ if ($token != md5(date('YmdH') . "D4teS4lt")
   array_push($err, $header);
 }
 
-// SendGrid integration to add the new form fillout to the appropriate list
-$curl = curl_init();
-$sendGridPayload = "{\"list_ids\":[\"16275b82-a697-4732-95f0-e6e6b2e6e41e\"],\"contacts\":[{\"email\":\"$email\",\"first_name\":\"$firstName\",\"last_name\":\"$lastName\",\"custom_fields\":{\"w9_T\":\"$startDate\",\"w10_T\":\"$endDate\",\"w7_T\":\"$service\",\"w8_T\":\"$message\"}}]}";
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.sendgrid.com/v3/marketing/contacts",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "PUT",
-  CURLOPT_POSTFIELDS => $sendGridPayload,
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer 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",
-    "content-type: application/json"
-  ),
-));
-
-$response = curl_exec($curl);
-$curlError = curl_error($curl);
-if (strlen($curlError)) {
-  // populate error array
-  array_push($err, curl_error($curlError));
-}
-curl_close($curl);
-
-// if there are any errors dump them and fail, else success!
+// if there are any errors dump them and fail, else succeed!
 if (count($err)) {
   header($header);
   echo "<meta http-equiv=\"refresh\" content=\"2;url=https://www.mazureth.com/booking.php#error\" />";
 } else {
+
+  // SendGrid integration to add the new form fillout to the appropriate list
+  $curl = curl_init();
+  $sendGridPayload = "{\"list_ids\":[\"16275b82-a697-4732-95f0-e6e6b2e6e41e\"],\"contacts\":[{\"email\":\"$email\",\"first_name\":\"$firstName\",\"last_name\":\"$lastName\",\"custom_fields\":{\"w9_T\":\"$startDate\",\"w10_T\":\"$endDate\",\"w7_T\":\"$service\",\"w8_T\":\"$message\"}}]}";
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.sendgrid.com/v3/marketing/contacts",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "PUT",
+    CURLOPT_POSTFIELDS => $sendGridPayload,
+    CURLOPT_HTTPHEADER => array(
+      "authorization: Bearer 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",
+      "content-type: application/json"
+    ),
+  ));
+
+  $response = curl_exec($curl);
+  $curlError = curl_error($curl);
+  if (strlen($curlError)) {
+    // populate error array
+    array_push($err, curl_error($curlError));
+  }
+  curl_close($curl);
+
   header($header);
   echo "<meta http-equiv=\"refresh\" content=\"2;url=https://www.mazureth.com/booking.php#thanks\" />";
 }
