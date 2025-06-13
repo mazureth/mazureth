8a4dc9383bcad2a90d65dc71ed10bfd744c341c1
Jesse Mazur <mazureth@gmail.com>
2025-06-13 13:23:19 -0700

    creating a 'link in bio' page for Insta

---
+++ lnkbio.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<?php include('./head-nav.php'); ?>
+
+<div class="container-fluid">
+    <div class="container">
+
+        <section id="lnkBio">
+
+            <div class="row" id="bioLinks">
+
+            </div>
+
+        </section>
+
+    </div>
+</div>
+
+
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<?php include('./footer.php'); ?>
+
+</html>
