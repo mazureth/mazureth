69b0b400cee363914f36e29a0c433256973aef01
Jesse Mazur <mazureth@gmail.com>
2025-12-22 11:04:26 -0800

    renderer now loads default or updates on input changes

--- diffuser.php
+++ diffuser.php
@@ -9,7 +9,12 @@
 
     <section id="diffuser">
       <h1 class="text-center">Diffuser Generator</h1>
-
+      <p>
+        Width <input type="number" id="plywoodWidthIn" value="48" min="1" max="96">
+        Height <input type="number" id="plywoodHeightIn" value="48" min="1" max="48">
+      </p>
+      <div id="renderedDiffuser">
+      </div>
     </section>
 
   </div>
